968cda18ba957bd19fc71a8b0226173bdc86dd67
Tem Tamre <temtamre2012@gmail.com>
2018-06-22 15:59:13 -0600

    Added a guide for using files

--- files/students.py
+++ files/students.py
@@ -6,6 +6,19 @@ Name:
 
 Description:
 Sources:
+
+===============================================
+Template on how to open and read a file
+===============================================
+file = open(filename)	# Opens the file and stores the file object in <file>
+file_contents = []		# Creates a list to store the lines in the file
+
+for line in file.readlines():	# Goes over each line in the file
+    file.contents.append(line)	# Appends each line in the file to <file_contents>
+    
+file.close()	# Closes the file, since we are now done with the file
+				# We still have access to all the lines in the file, since we stored them in <file_contents>
+===============================================
 """
 
 
